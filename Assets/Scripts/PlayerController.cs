3dc7865d68d77d49fd3ed9d8667ff50c14cc6f50
viviancan <vcnales@gmail.com>
2021-02-26 18:54:34 -0600

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    // check every frame for player input
+    // apply that to player game object as movement
+    // fixed update <= called just before physics calculations
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementVal)
+    {
+        //getting input value from player
+        // gets vector 2 data from movement value and stores it into the vector2 variable = movementVector
+        Vector2 movementVector = movementVal.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+        //Add force to the 
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement);
+    }
+}
