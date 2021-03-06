e37a7dbeb80ef093170243f6461df2d83a44e77b
viviancan <vcnales@gmail.com>
2021-03-06 16:25:31 -0600

    completed game

---
+++ Assets/Scripts/SimHandMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandMove : MonoBehaviour
+{
+    public Transform location;
+    public Vector3 position;
+    public float moveSpeed;
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) | Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) | Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * turnSpeed);
+        }
+        
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed);
+        }
+    }
+}
